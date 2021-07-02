ec8f6790ef09c56383678ca426c64f31bf66e59c
Richard Mamani <richard.mamani@tecsup.edu.pe>
2021-07-02 03:28:03 -0500

    Initial commit

---
+++ app/src/main/java/tecsup/edu/recuperacion/Api.kt
@@ -0,0 +1,13 @@
+package tecsup.edu.recuperacion
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface Api {
+    @GET("movie/upcoming")
+    fun getUpcomingMovies(
+        @Query("api_key") apiKey: String = "c9c5ea53799624204822e99e30c87b54",
+        @Query("page") page: Int
+    ): Call<GetMoviesResponse>
+}
